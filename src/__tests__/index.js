d9cd2f1d9c2e7950b62e4f2adb23d84a487c6a02
Bruno Novais <cardososp@gmail.com>
2022-04-11 11:49:43 -0300

    test(scaffolding): Add scaffolding before start of project

---
+++ src/__tests__/index.js
@@ -0,0 +1,12 @@
+const math = require('../math');
+
+const cases = require('jest-in-case');
+
+
+cases('add(augend, addend)', opts => {
+  expect(math.add(opts.augend, opts.addend)).toBe(opts.total);
+}, [
+  { name: '1 + 1 = 2', augend: 1, addend: 1, total: 2 },
+  { name: '2 + 1 = 3', augend: 2, addend: 1, total: 3 },
+  { name: '3 + 1 = 4', augend: 3, addend: 1, total: 4 },
+]);
---
+++ src/math.js
@@ -0,0 +1,9 @@
+
+export function add(augend, addend) {
+    return augend + addend;
+}
+  
+export function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
